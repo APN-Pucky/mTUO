31159912fd4686374911f37178fdc80bb1e082de
Declan Moran <dec1@geoaze.com>
2017-09-22 16:12:49 +0200

    improved doIt.sh

--- doIt.sh
+++ doIt.sh
@@ -2,41 +2,65 @@
 
 # Extract boost (src) archive to a directory of the form "major.minor.patch" 
 # so that the dir name ~  boost version (eg "1.64.0")
-
-
+#---------------------------------------------------------------------------------
 # Example script. Modify the variables below as appropriate for your local setup.
 #---------------------------------------------------------------------------------
 
-# Specify the path to boost src dir 
+
+
+
+
+# Specify the path to boost source code dir 
 BOOST_SRC_DIR=/home/declan/Documents/zone/mid/lib/boost/down
 
-# Specify the version of boost in the source dir
+#------------------------------------------------------------------------------------------
+# Specify the version of boost youre building
 #BOOST_VERSION=1.64.0
 BOOST_VERSION=1.65.1
 
-# Specify path to (Google) Ndk
-#GOOGLE_DIR=/home/declan/Documents/zone/mid/lib/android/sdk/ndk-bundle
-#GOOGLE_DIR=/home/declan/Documents/zone/mid/lib/android/ndk/15c/android-ndk-r15c
-GOOGLE_DIR=/home/declan/Documents/zone/mid/lib/android/ndk/16b1/android-ndk-r16-beta1
+#------------------------------------------------------------------------------------------
+# Specify path to the (Google) Ndk  (by default  downloded to "..sdk/ndk-bundle" by android studio)
+#ANDROID_NDK_ROOT=/home/declan/Documents/zone/mid/lib/android/ndk/15c/android-ndk-r15c
+export ANDROID_NDK_ROOT=/home/declan/Documents/zone/mid/lib/android/ndk/16b1/android-ndk-r16-beta1
 
-
-# Dont modify
-export ANDROID_NDK_ROOT=$GOOGLE_DIR
-
+#------------------------------------------------------------------------------------------
 # Modify if desired
 # log file where build messages will be stored
 logFile=build_out.txt
 rm $logFile
 
-
+#------------------------------------------------------------------------------------------
 # the build script writes some extra info to this file if its defined (but will be in "split" across different dirs)
 #export NDK_LOGFILE=ndk_log_out.txt
 
 
-./build_tools/build-boost.sh --version=1.65.1 --stdlibs="gnu-4.9" --abis="armeabi-v7a"  --ndk-dir=$ANDROID_NDK_ROOT --linkage="shared" --verbose $BOOST_SRC_DIR  2>&1 | tee -a $logFile
 
-#./build_tools/build-boost.sh --version=1.65.1 --stdlibs="gnu-4.9, llvm-3.5" --abis="armeabi-v7a, x86"  --ndk-dir=$ANDROID_NDK_ROOT --linkage="shared,static" --verbose $BOOST_SRC_DIR  2>&1 | tee -a $logFile 
-# ./build_tools/build-boost.sh --version=1.64.0 --stdlibs="gnu-4.9" --abis="armeabi-v7a"  --ndk-dir=$ANDROID_NDK_ROOT --linkage="shared" --verbose $BOOST_SRC_DIR  2>&1 | tee -a $logFile
+# The options --stdlibs, --abis and --linkage can be one or more of the listed posible values. If you specify more than one, then separate individual values by a comma
+#------------------------------------------------------------------------------------------
+# which compiler to use       
+# possible values:   {llvm-3.5, gnu-4.9}
+STD_LIBS="llvm-3.5"              
+# STD_LIBS="llvm-3.5, gnu-4.9"    
+
+# which abis (~ architecture + instruction set) to build for     
+# possible values:  {armeabi-v7a, x86, mips}
+ABIS="armeabi-v7a"              
+# ABIS="armeabi-v7a, x86, mips"
+
+# whether to build shared or static libraries (or both)          
+# possible values:   {shared, static}
+LINKAGE="shared"               
+#LINKAGE="shared, static"
+
+
+
+#--------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
+# Dont modify  - the actual call
+#-------------------------------
+
+./build_tools/build-boost.sh --version=$BOOST_VERSION --stdlibs=$STD_LIBS --abis=$ABIS  --ndk-dir=$ANDROID_NDK_ROOT --linkage=$LINKAGE --verbose $BOOST_SRC_DIR  2>&1 | tee -a $logFile
+
 
 
 
